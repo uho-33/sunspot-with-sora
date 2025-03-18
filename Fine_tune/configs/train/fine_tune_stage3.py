e9be2dc8f906acc4dc6f05c6ce90d29eaca9301a
Yufan Ma <1392693274@qq.com>
2025-03-19 00:38:38 +0800

    decrease lr and warmup_step use larger batch size

--- Fine_tune/configs/train/fine_tune_stage3.py
+++ Fine_tune/configs/train/fine_tune_stage3.py
@@ -24,7 +24,7 @@ plugin = "zero1"
 # Model settings
 model = dict(
     type="Sunspot_STDiT3-XL/2",
-    from_pretrained="outputs/0003-Sunspot_STDiT3-XL-2/epoch119-global_step600",
+    from_pretrained="outputs/0001-Sunspot_STDiT3-XL-2/epoch457-global_step21500/ema.pt",
     qk_norm=True,
     enable_flash_attn=True,
     enable_layernorm_kernel=False,
@@ -34,8 +34,8 @@ model = dict(
     use_spatial_rope=True,
     freeze_other=False, 
     init_cross_attn=False,
-    orig_mapping_size=256,
-    new_mapping_size=1024,
+    # orig_mapping_size=256,
+    # new_mapping_size=1024,
 )
 vae = dict(
     type="OpenSoraVAE_V1_3",
@@ -73,7 +73,7 @@ ckpt_every = 200
 
 # optimization settings
 lr = 2e-5
-warmup_steps = 500
+warmup_steps = 100
 grad_clip = 1.0
 adam_eps = 1e-15
 ema_decay = 0.99
